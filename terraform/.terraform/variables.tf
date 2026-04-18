ecda9536685c8915df5c70f0219e77bbc2d193bd
mahder05 <thegenius.mahesh@gmail.com>
2026-04-18 08:04:04 +0200

    feat: testing vault credentials fetch using vars file on linux hosts...

--- terraform/.terraform/variables.tf
+++ terraform/.terraform/variables.tf
@@ -1,11 +1,22 @@
+variable "vault_role_id" {
+  description = "The Role ID for the Vault AppRole"
+  type        = string
+}
+
+variable "vault_approle_secret_id" {
+  description = "The Secret ID for the Vault AppRole"
+  type        = string
+  sensitive   = true
+}
+
 variable "awx_password" {
-  description = "The admin password for AWX"
+  description = "Admin password for AWX"
   type        = string
   sensitive   = true
 }
 
 variable "vault_root_token" {
-  description = "The initial root token for Vault"
+  description = "The root token for Vault (used for manual bootstrapping)"
   type        = string
   sensitive   = true
 }
